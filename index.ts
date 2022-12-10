c25799034f60a149a4596f1221e3fcb43a405237
Chris Young <chris.young@brighte.com.au>
2022-12-10 19:45:31 +1100

    day 4

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import { readFileSync } from "fs";
 import path from "path";
-import { part1, part2 } from "./day3";
+import { part1, part2 } from "./day4";
 
-const input = readFileSync(path.join(__dirname, 'day3.txt')).toString().trim();
+const input = readFileSync(path.join(__dirname, 'day4.txt')).toString().trim();
 
 console.log(part1(input))
 console.log(part2(input))
