dff04c97566419c6ed4966b2696ff28a2e4227f0
Chris Young <chris.young@brighte.com.au>
2022-12-10 12:44:04 +1100

    day 2

---
+++ day2.spec.ts
@@ -0,0 +1,16 @@
+import { part1 } from "./day2"
+
+const input = `
+A Y
+B X
+C Z
+`
+
+describe('day 2', () => {
+    test('part1', () => {
+        expect (part1(input.trim())).toEqual(15)
+    })
+    test('part2', () => {
+        expect (part1(input.trim())).toEqual(15)
+    })
+})
